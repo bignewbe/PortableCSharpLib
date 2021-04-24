76e81c23e7285d0400b66913dc8a0c20a18431a5
Ping Li <imliping@gmail.com>
2021-04-24 15:04:53 +0800

    added MyException

--- PortableCSharpLib/Exceptions.cs
+++ PortableCSharpLib/Exceptions.cs
@@ -5,6 +5,22 @@ using System.Text;
 
 namespace PortableCSharpLib
 {
+    public class MyException : Exception
+    {
+        public string Code { get; set; }
+        //public string Message { get; set; }
+        public MyException(string errorCode, string message) : base(message)
+        {
+            this.Code = errorCode;
+            //this.Message = message;
+        }
+        public MyException(string code, string message, Exception innerException) : base(message, innerException)
+        {
+            this.Code = code;
+            //this.Message = message;
+        }
+    }
+
     public class FileFormatNotSupportedException : Exception
     {
         public FileFormatNotSupportedException(string msg) : base(msg)
