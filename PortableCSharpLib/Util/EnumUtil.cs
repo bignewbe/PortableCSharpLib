4ca4bc4b78fd45828bfa9e6e7b665ab220545f25
Ping Li <imliping@gmail.com>
2021-09-10 01:40:44 +0800

    add EnumUtil.c

---
+++ PortableCSharpLib/Util/EnumUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PortableCSharpLib.Util
+{
+    public static class UtilExtension
+    {
+        public static T ParseEnum<T>(this string value, T defaultValue) where T : struct
+        {
+            if (string.IsNullOrEmpty(value))
+                return defaultValue;
+
+            T result;
+            return Enum.TryParse<T>(value, true, out result) ? result : defaultValue;
+        }
+    }
+}
