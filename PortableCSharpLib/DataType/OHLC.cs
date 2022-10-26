9715b2e0abb157620ab27fd22cf1b40b0c083e0d
Ping Li <imliping@gmail.com>
2022-10-26 14:28:25 +0200

    1. optimize OHLC for transmission

--- PortableCSharpLib/DataType/OHLC.cs
+++ PortableCSharpLib/DataType/OHLC.cs
@@ -1,19 +1,29 @@
-﻿using PortableCSharpLib.Interface;
+﻿using Newtonsoft.Json;
+using PortableCSharpLib.Interface;
 using System;
 
 namespace PortableCSharpLib.DataType
 {
     public class OHLC : IIdEqualCopy<OHLC>
     {
-        public string Symbol { get; set; }
-        public int Interval { get; set; }
-        public long Time { get; set; }
-        public double Open { get; set; }
-        public double High { get; set; }
-        public double Low { get; set; }
-        public double Close { get; set; }
-        public double Volume { get; set; }
+        [JsonIgnore]
         public string Id { get { return this.Symbol; } }
+        [JsonProperty(PropertyName = "S")]
+        public string Symbol { get; set; }
+        [JsonProperty(PropertyName = "I")]
+        public int Interval { get; set; }
+        [JsonProperty(PropertyName = "T")]
+        public long Time { get; set; }
+        [JsonProperty(PropertyName = "O")]
+        public double Open { get; set; }
+        [JsonProperty(PropertyName = "H")]
+        public double High { get; set; }
+        [JsonProperty(PropertyName = "P")]
+        public double Low { get; set; }
+        [JsonProperty(PropertyName = "C")]
+        public double Close { get; set; }
+        [JsonProperty(PropertyName = "V")]
+        public double Volume { get; set; }
 
         public OHLC() { }
         public OHLC(OHLC other) => this.Copy(other);
